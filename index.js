43c0184037d4c2cf53d83f5ef161555d54a47e15
Muniza .. <159540004+Muniza-NAbeel@users.noreply.github.com>
2024-05-23 15:31:14 +0500

    work complete

---
+++ index.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("*** Most Welcome ***");
+let myTable = true;
+// Our Full Code Will be in While Loop
+while (myTable) {
+    let myInput = await inquirer.prompt([
+        {
+            name: "num",
+            type: "number",
+            message: "Enter Your Number"
+        }
+    ]);
+    let myNum = myInput.num;
+    if (myNum) {
+        console.log(`Here is the Table Of ${myNum}`);
+        for (let i = 1; i <= 10; i++) {
+            console.log(`${myNum} X ${i} = ${myNum * i}`);
+        }
+    }
+    else {
+        console.log("Please Enter input in Numerical Form");
+    }
+    // Asking User If he Wants tO Print Table Again
+    let againPrint = await inquirer.prompt([
+        {
+            name: "askUsr",
+            type: "confirm",
+            message: "Do You Want to Print Another Table??",
+            default: false
+        }
+    ]);
+    if (againPrint.askUsr === false) {
+        myTable = false;
+        console.log(" \n Thank You For Using This Table Generator Program .. ");
+    }
+}
